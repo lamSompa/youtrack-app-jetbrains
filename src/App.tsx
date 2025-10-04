d693e76dba856a2ea6fff5b5cae6200f03f9c65a
Abeiku Sompa Nyarko-Lartey <abeikusompanyarkolartey@gmail.com>
2025-10-05 00:32:55 +0200

    feat: integrated Ring UI Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+   import React from 'react';
+   import Button from '@jetbrains/ring-ui/components/button/button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+   function App() {
+     return (
+       <div style={{ padding: 20 }}>
+         <h1>Hello, YouTrack!</h1>
+         <Button primary>Test Ring UI Button</Button>
+       </div>
+     );
+   }
 
-export default App;
+   export default App;
